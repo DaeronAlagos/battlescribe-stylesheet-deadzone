a0f173e6bc3d22b31e274e98ed012622bfcd9c09
James Curtis <mybastion@gmail.com>
2022-12-18 13:53:44 +0000

    Update grunt tooling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,74 @@
+const sass = require('node-sass')
+
 module.exports = function(grunt) {
     grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass,
+                soureMap: true,
+            },
+            dist: {
+                files: {
+                    'build/style.css': 'src/scss/style.scss'
+                }
+            }
+        },
+        xsltproc: {
+            options: {
+                stylesheet: 'build/base.xsl'
+            },
+            compile: {
+                files: {
+                    'build/template.html': ['data/nameless-rush.ros'],
+                }
+            }
+        },
         browserSync: {
             bsFiles: {
-                src: 'src/*'
+                src: 'build/template.html'
             },
             options: {
                 server: {
-                    baseDir: 'src',
-                    index: 'index.html',
-                }
+                    baseDir: 'build',
+                    index: 'template.html',
+                },
+                watchTask: true,
+            }
+        },
+        watch: {
+            template: {
+                files: ['src/base.xsl', 'src/scss/style.scss'],
+                tasks: ['sass', 'replace', 'xsltproc']
+            },
+        },
+        replace: {
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'include_style',
+                            replacement: '<%= grunt.file.read("build/style.css") %>',
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/base.xsl'],
+                        dest: 'build/'
+                    }
+                ]
             }
         }
 
     })
 
     grunt.loadNpmTasks('grunt-browser-sync')
-
-    grunt.registerTask('default', ['browserSync'])
+    grunt.loadNpmTasks('grunt-xsltproc')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-sass')
+    grunt.loadNpmTasks('grunt-replace')
+    
+    grunt.registerTask('default', ['browserSync', 'watch'])
 }
